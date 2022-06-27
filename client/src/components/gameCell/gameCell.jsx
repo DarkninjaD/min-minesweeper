6bea10553c3ceea74f9bd9b4cdceb7eba9ec0cdd
Dayan Sauerbronn <d.sauerbronn@gmail.com>
2022-06-27 08:33:49 -0500

    gameCells

--- client/src/components/gameCell/gameCell.jsx
+++ client/src/components/gameCell/gameCell.jsx
@@ -1,30 +1,61 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import styled from "styled-components";
 import { useAppContext } from "../../context/appContext";
 
-const GameCell = ({ key }) => {
+const GameCell = ({ refObj }) => {
+  const [clicked, setClicker] = useState(false);
   const { setGameCells, gameCells } = useAppContext();
+  const bombCheck = (e) => {
+    setClicker(true);
+    if (!refObj.bomb) {
+      // bombLeft(refObj.location)
+      // bombRight(refObj.location)
+      // bombUp(refObj.location)
+      // bombDown(refObj.location)
+    }
+  };
 
-  // useEffect(() => {
-  //   setGameCells((gameCells) => [
-  //     ...gameCells,
-  //     {
-  //       location: key,
-  //       bomb: false,
-  //       tally: 0,
-  //     },
-  //   ]);
-  // }, [key]);
-
-  return <GameCellStyle />;
+  return (
+    <>
+      {clicked ? (
+        refObj.bomb ? (
+          <GameCellBomb />
+        ) : (
+          <GameCellSafe> {refObj.tally}</GameCellSafe>
+        )
+      ) : (
+        <GameCellHidden onClick={(e) => bombCheck(e)} />
+      )}
+    </>
+  );
 };
 
 export default GameCell;
-const GameCellStyle = styled.div`
+const GameCellHidden = styled.div`
   display: flex;
   justify-self: center;
   align-self: center;
   width: 64px;
   height: 64px;
   background-color: black;
+`;
+
+const GameCellBomb = styled.div`
+  display: flex;
+  justify-self: center;
+  align-self: center;
+  width: 64px;
+  height: 64px;
+  background-color: #ff0000;
+`;
+
+const GameCellSafe = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  justify-self: center;
+  align-self: center;
+  width: 64px;
+  height: 64px;
+  background-color: #090761;
 `;
--- client/src/components/gameGrid/gameGrid.jsx
+++ client/src/components/gameGrid/gameGrid.jsx
@@ -11,9 +11,7 @@ const GameGrid = () => {
     min-height: 824px;
     grid-template-columns: repeat(${difficult.gridSize[1]}, 1fr);
     grid-gap: 8px;
-    /* grid-template-columns: repeat(10, ); */
     grid-auto-rows: 8vh;
-    /* grid-template-rows: ${difficult.gridSize[0]}; */
   `;
 
   const totalSize = difficult.gridSize[0] * difficult.gridSize[1];
@@ -21,15 +19,16 @@ const GameGrid = () => {
   const allCells = [];
   const allGameCells = [];
   for (let i = 0; i < totalSize; i++) {
-    allCells.push(<GameCell key={i + 1} />);
     allGameCells.push({
       location: i,
       bomb: false,
       tally: 0,
     });
+    allCells.push(<GameCell key={i} refObj={allGameCells[i]} />);
   }
   useEffect(() => {
     setGameCells(allGameCells);
+    console.log(gameCells);
   }, []);
 
   return <GridContainer>{allCells.map((elm) => elm)}</GridContainer>;
